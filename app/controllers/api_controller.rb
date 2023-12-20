fb26a89f687d38090d541f84d09a50d43f75fe97
Abdoul <abdoulramanediallo44@gmail.com>
2023-12-20 12:59:13 +0000

    adding method limited and unlimited

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,16 @@
 class ApiController < ApplicationController
-    before_action :rate_limite, only: [:limited]
+    before_action :rate_limit, only: [:limited]
 
+    def limited
+        render json: {message: 'This is a limited endpoint'}
+    end
+
+    def unlimited
+        render json: {message: 'This is an unlimited endpoint'}
+    end
+
+    private
+    def rate_limit
+        
+    end
 end
